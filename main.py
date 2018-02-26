a96dbda3daa9e50df51c328d680398b37e0cbb32
David Wallach <davidwallach2@gmail.com>
2018-02-26 00:09:52 -0600

    finished parsing code

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ Recipe Transformer Project
 
 Given a URL to a recipe from AllRecipes.com, this program uses Natural Language Processing to transform any recipe, based on
 user's input, into any or all of the following categories:  
-	•   To and from vegetarian and/or vegan
-	•   Style of cuisine
-	•   To and from healthy (or perhaps even different types of healthy)
-	•   DIY to easy
-	•   Cooking method (from bake to stir fry, for example)
+	*   To and from vegetarian and/or vegan
+	*   Style of cuisine
+	*   To and from healthy (or perhaps even different types of healthy)
+	*   DIY to easy
+	*   Cooking method (from bake to stir fry, for example)
 
 
 Authors: 
- 	• David Wallach
+ 	* David Wallach
 
 """
-
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -24,17 +24,40 @@ class Recipe(object):
 	Used to represent a recipe. Data for each recipe can be found 
 	on AllRecipes.com. 
 	"""
-	def __init__(self):
-		pass
+	def __init__(self, **kwargs):
+		for key, value in kwargs.items():
+			setattr(self, key, value)
+
+		
 
 
-
+def remove_non_numerics(string): return re.sub('[^0-9]', '', string)
 
 
 def parse_url(url):
 	"""
 	reads the url and creates a recipe object.
 	Urls are expected to be from AllRecipes.com
+
+
+	Builds a dictionary that is passed into a Recipe object's init function and unpacked.
+	The dictionary is set up as 
+
+	{
+		name: string
+		preptime: int
+		cooktime: int
+		totaltime: int
+		ingredients: list of strings
+		directions: list of strings
+		calories: int
+		carbs: int
+		fat: int
+		protien: int
+		cholesterol: int
+		sodium: int
+
+	}
 	"""
 
 	# retrieve data from url
@@ -43,6 +66,68 @@ def parse_url(url):
 
 	# store in BeautifulSoup object to parse HTML DOM
 	soup = BeautifulSoup(c, "lxml")
+
+
+	# find name 
+	name = soup.find('h1', {'itemprop': 'name'}).text
+	
+	# find relavent time information
+	preptime  = remove_non_numerics(soup.find('time', {'itemprop': 'prepTime'}).text)
+	cooktime  = remove_non_numerics(soup.find('time', {'itemprop': 'cookTime'}).text)
+	totaltime = remove_non_numerics(soup.find('time', {'itemprop': 'totalTime'}).text)
+	
+	# find ingredients
+	ingredients = [i.text for i in soup.find_all('span', {'class': 'recipe-ingred_txt added'})]
+
+	# find directions
+	directions = [i.text for i in soup.find_all('span', {'class': 'recipe-directions__list--item'})] 
+
+
+	# nutrition facts
+	calories = remove_non_numerics(soup.find('span', {'itemprop': 'calories'}).text)				
+	carbs = soup.find('span', {'itemprop': 'carbohydrateContent'}).text			    # measured in grams
+	fat = soup.find('span', {'itemprop': 'fatContent'}).text						# measured in grams
+	protien  = soup.find('span', {'itemprop': 'proteinContent'}).text			    # measured in grams
+	cholesterol  = soup.find('span', {'itemprop': 'cholesterolContent'}).text	    # measured in miligrams
+	sodium  = soup.find('span', {'itemprop': 'sodiumContent'}).text			        # measured in grams
+
+		
+
+	# print ('recipe is called {}'.format(name))
+	# print ('prep time is {} minutes, cook time is {} minutes and total time is {} minutes'.format(preptime, cooktime, totaltime))
+	# print ('it has {} ingredients'.format(len(ingredients)))
+	# print ('it has {} directions'.format(len(directions)))
+	# print ('it has {} calories, {} g of carbs, {} g of fat, {} g of protien, {} mg of cholesterol, {} mg of sodium'.format(calories, carbs, fat, protien, cholesterol, sodium))
+
+
+	return {
+			'name': name,
+			'preptime': preptime,
+			'cooktime': cooktime,
+			'totaltime': totaltime,
+			'ingredients': ingredients,
+			'directions': directions,
+			'calories': calories,
+			'carbs': carbs,
+			'fat': fat,
+			'protien': protien,
+			'cholesterol': cholesterol,
+			'sodium': sodium
+
+			}
+
+
+
+def main():
+	# test_url = 'http://allrecipes.com/recipe/234667/chef-johns-creamy-mushroom-pasta/?internalSource=rotd&referringId=95&referringContentType=recipe%20hub'
+	test_url = 'http://allrecipes.com/recipe/21014/good-old-fashioned-pancakes/?internalSource=hub%20recipe&referringId=1&referringContentType=recipe%20hub'
+	recipe_attrs = parse_url(test_url)
+	recipe = Recipe(**recipe_attrs)
+
+
+
+if __name__ == "__main__":
+	main()
 
 
 
